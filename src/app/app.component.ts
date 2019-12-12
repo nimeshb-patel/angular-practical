6599849e56c0282b73d432f7de5d7740f2b99300
Nimesh <nimeshb.patel@tatvasoft.com>
2019-12-12 16:33:00 +0530

    table, filter, and api call added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatTableDataSource } from '@angular/material';
+
+import { timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { ApiService } from './services/api.service';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-practical';
+export class AppComponent implements OnInit {
+
+    hits: MatTableDataSource<any>;
+    displayedColumns: string[] = ['title', 'url', 'created_at', 'author'];
+
+    constructor(private apiService: ApiService, private matDialog: MatDialog) {
+
+    }
+
+    ngOnInit() {
+        this.hits = new MatTableDataSource([]);
+        this.hits.filterPredicate = function (table_data: any, filter: string): boolean {
+            return table_data.title.toLowerCase().includes(filter);
+        };
+
+        timer(0, 10000).pipe(
+            switchMap(() => this.apiService.getData())
+        ).subscribe((data) => {
+            this.hits = new MatTableDataSource(data.hits);
+        }, (error) => {
+            console.error(error);
+        });
+    }
+
+    openDialog(row: any) {
+        this.matDialog.open(ModalComponent, {
+            width: '700px',
+            data: row
+        });
+    }
+
+    applyFilter(filterkey: string) {
+        this.hits.filter = filterkey.trim().toLowerCase();
+    }
 }
